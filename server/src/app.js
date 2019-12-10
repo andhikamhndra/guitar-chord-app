d36f510267421e44fa5299ba2213b485044decf7
Andhika Mahendra <andhikamhndra@gmail.com>
2019-12-10 15:50:52 +0700

    add express js server

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world!'
+    })
+})
+
+app.listen(process.env.PORT || 8801)
